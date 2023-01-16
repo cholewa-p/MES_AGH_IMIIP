0dafe2e3eb6c48f4609d01a3a4125c823e0467e3
cholewa-p <pawel.cholewa@o2.pl>
2023-01-16 22:20:40 +0100

    removed duplicates

--- MES/Jakobian.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "Elem4.h"
-#include "Grid.h"
-#include <iostream>
-using namespace std;
-void jakobian(int i, int j, double I[2][2], double Iinv[2][2], Elem4 element, Grid& grid);
