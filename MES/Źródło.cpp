c833593abe8837bf4470505f84152cb6076b93a4
cholewa-p <pawel.cholewa@o2.pl>
2023-01-16 22:11:05 +0100

    new fixes

--- MES/Źródło.cpp
+++ MES/Źródło.cpp
@@ -20,12 +20,12 @@ int main()
 	Elem4 element(2);
 	//element.show_Elem4();
 	calculate_matrix_H(grid, element);
-	grid.show_H_Matrix();
+	//grid.show_H_Matrix();
 	calculate_matrix_Hbc(grid,element);
 	double* P = aggregate_vector_P(grid);
 	grid.initialise_Temperature();
 	grid.sum_BC();
-	grid.show_H_Matrix();
+	//grid.show_H_Matrix();
 	double** H = aggregate_vector_H(grid);
 	calculate_matrix_C(grid,element);
 	double** C = aggregate_vector_C(grid);
